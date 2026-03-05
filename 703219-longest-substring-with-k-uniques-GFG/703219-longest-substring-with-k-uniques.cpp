d77ec1ad8e6fa22a82251f0c8719d3e055651b52
Ramkumar dehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2026-03-05 08:45:10 +0530

    Added solution - LeetHub

---
+++ 703219-longest-substring-with-k-uniques-GFG/703219-longest-substring-with-k-uniques.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int longestKSubstr(string &s, int k) {
+        // code here
+        map<char, int> mp;
+        int l = 0, r = 0, n = s.size(), ans = 0;
+        while(r < n){
+            mp[s[r]]++;
+            while (mp.size() > k){
+                mp[s[l]]--;
+                if (mp[s[l]] == 0) mp.erase(s[l]);
+                l++;
+            }
+            if (mp.size() == k) ans = max(ans, r-l+1);
+            r++;
+        }
+        return ans == 0 ? -1 : ans;
+    }
+};
